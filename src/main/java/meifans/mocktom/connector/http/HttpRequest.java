2c2ff34f2da5254d15dc5dc3d5adb1d92701dc67
赵鹏飞 <pengfeizhao9@creditease.cn>
2016-07-11 14:09:01 +0800

    fix warning

--- src/main/java/meifans/mocktom/connector/http/HttpRequest.java
+++ src/main/java/meifans/mocktom/connector/http/HttpRequest.java
@@ -16,173 +16,144 @@ public class HttpRequest implements ServletRequest {
     public HttpRequest(SocketInputStream input) {
     }
 
-    @Override
     public Object getAttribute(String arg0) {
 
         return null;
     }
 
-    @Override
     public Enumeration getAttributeNames() {
 
         return null;
     }
 
-    @Override
     public String getCharacterEncoding() {
 
         return null;
     }
 
-    @Override
     public int getContentLength() {
 
         return 0;
     }
 
-    @Override
     public String getContentType() {
 
         return null;
     }
 
-    @Override
     public ServletInputStream getInputStream() throws IOException {
 
         return null;
     }
 
-    @Override
     public String getLocalAddr() {
 
         return null;
     }
 
-    @Override
     public String getLocalName() {
 
         return null;
     }
 
-    @Override
     public int getLocalPort() {
 
         return 0;
     }
 
-    @Override
     public Locale getLocale() {
 
         return null;
     }
 
-    @Override
     public Enumeration getLocales() {
 
         return null;
     }
 
-    @Override
     public String getParameter(String arg0) {
 
         return null;
     }
 
-    @Override
     public Map getParameterMap() {
 
         return null;
     }
 
-    @Override
     public Enumeration getParameterNames() {
 
         return null;
     }
 
-    @Override
     public String[] getParameterValues(String arg0) {
 
         return null;
     }
 
-    @Override
     public String getProtocol() {
 
         return null;
     }
 
-    @Override
     public BufferedReader getReader() throws IOException {
 
         return null;
     }
 
-    @Override
     public String getRealPath(String arg0) {
 
         return null;
     }
 
-    @Override
     public String getRemoteAddr() {
 
         return null;
     }
 
-    @Override
     public String getRemoteHost() {
 
         return null;
     }
 
-    @Override
     public int getRemotePort() {
 
         return 0;
     }
 
-    @Override
     public RequestDispatcher getRequestDispatcher(String arg0) {
 
         return null;
     }
 
-    @Override
     public String getScheme() {
 
         return null;
     }
 
-    @Override
     public String getServerName() {
 
         return null;
     }
 
-    @Override
     public int getServerPort() {
 
         return 0;
     }
 
-    @Override
     public boolean isSecure() {
 
         return false;
     }
 
-    @Override
     public void removeAttribute(String arg0) {
 
     }
 
-    @Override
     public void setAttribute(String arg0, Object arg1) {
 
     }
 
-    @Override
     public void setCharacterEncoding(String arg0) throws UnsupportedEncodingException {
 
     }
--- src/main/java/meifans/mocktom/connector/http/HttpResponse.java
+++ src/main/java/meifans/mocktom/connector/http/HttpResponse.java
@@ -14,84 +14,69 @@ public class HttpResponse implements ServletResponse {
         // TODO Auto-generated constructor stub
     }
 
-    @Override
     public void flushBuffer() throws IOException {
 
     }
 
-    @Override
     public int getBufferSize() {
 
         return 0;
     }
 
-    @Override
     public String getCharacterEncoding() {
 
         return null;
     }
 
-    @Override
     public String getContentType() {
 
         return null;
     }
 
-    @Override
     public Locale getLocale() {
 
         return null;
     }
 
-    @Override
     public ServletOutputStream getOutputStream() throws IOException {
 
         return null;
     }
 
-    @Override
     public PrintWriter getWriter() throws IOException {
 
         return null;
     }
 
-    @Override
     public boolean isCommitted() {
 
         return false;
     }
 
-    @Override
     public void reset() {
 
     }
 
-    @Override
     public void resetBuffer() {
 
     }
 
-    @Override
     public void setBufferSize(int arg0) {
 
     }
 
-    @Override
     public void setCharacterEncoding(String arg0) {
 
     }
 
-    @Override
     public void setContentLength(int arg0) {
 
     }
 
-    @Override
     public void setContentType(String arg0) {
 
     }
 
-    @Override
     public void setLocale(Locale arg0) {
 
     }

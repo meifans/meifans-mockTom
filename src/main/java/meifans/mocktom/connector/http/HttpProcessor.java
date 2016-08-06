35a4bf22c4665e048cb279d63ce5e7cbd7858dff
meifans <916109363@qq.com>
2016-08-06 11:52:18 +0800

    refactor httpProcessor

--- src/main/java/meifans/mocktom/connector/http/HttpProcessor.java
+++ src/main/java/meifans/mocktom/connector/http/HttpProcessor.java
@@ -10,12 +10,14 @@ import org.apache.tomcat.util.res.StringManager;
 
 import meifans.mocktom.container.ServletProcessor;
 import meifans.mocktom.container.StaticResourceProcessor;
+import org.jetbrains.annotations.NotNull;
 
 public class HttpProcessor {
 
     private HttpConnector connector;
     private HttpRequest request;
     private HttpResponse response;
+    //没有完成
     private HttpRequestLine requestLine = new HttpRequestLine();
 
     private static final String MATCH_SESSION_ID = ";jsessionid=";
@@ -34,25 +36,29 @@ public class HttpProcessor {
         SocketInputStream input = null;
         OutputStream output = null;
         try {
+            //read 方法重载没有完成
+            //取得输入输出流
             input = new SocketInputStream(socket.getInputStream(), 2048);
             output = socket.getOutputStream();
 
+            //httpRequest和httpResponse没有完成
+            //创建，初始化request与response对象
             request = new HttpRequest(input);
             response = new HttpResponse(output);
             response.setRequest(request);
             response.setHeader("Server", "mockTom Servlet container");
 
             parseRequest(input, output);
-            pareHeaders(input);
+            pareHeaders(input);//没有完成
 
             // check if this is a request for a servlet or a static resource
             // a request for a servlet begin with "/servlet/"
             if (request.getRequestURI().startsWith("/servlet/")) {
                 ServletProcessor processor = new ServletProcessor();
-                processor.process(request, response);
+                processor.process(request, response);//没有完成
             } else {
                 StaticResourceProcessor processor = new StaticResourceProcessor();
-                processor.process(request, response);
+                processor.process(request, response);//没有完成
             }
 
             socket.close();
@@ -89,17 +95,7 @@ public class HttpProcessor {
         } else if (requestLine.uriEnd < 1) {
             throw new ServletException("Missing HTTP request URI");
         }
-        // Parse any query parameters out of the request URI
-        int question = requestLine.indexOf("?");
-        if (question >= 0) {
-            request.setQueryString(new String(requestLine.uri, question + 1,
-                    requestLine.uriEnd));
-            uri = new String(requestLine.uri, 0, question);
-        } else {
-            request.setQueryString(null);
-            uri = new String(requestLine.uri, 0, requestLine.uriEnd);
-        }
-
+        uri = parseParameters();
         // Checking for an absolute URI (with the HTTP protocol)
         if (!uri.startsWith("/")) {
             int pos = uri.indexOf("://");
@@ -113,7 +109,28 @@ public class HttpProcessor {
                 }
             }
         }
+        uri = sessionID(uri);
 
+
+        // Normalize URI (using String operation at the moment)
+        String normalizedUri = normalize(uri);
+        // Set the corresponding request properties.
+        // Transition protect set HttpRequest's properties,not subclass's.
+        // If it have.
+        ((HttpRequest) request).setMethod(method);
+        request.setProtocol(protocol); // I also can't understand this ...
+        if (normalizedUri != null) {
+            ((HttpRequest) request).setRequestURI(normalizedUri);
+        } else {
+            ((HttpRequest) request).setRequestURI(uri);
+        }
+
+        if (normalizedUri == null) {
+            throw new ServletException("Invalid URI:" + uri + "'");
+        }
+    }
+
+    private String sessionID(String uri) {
         // Parse any requested session Id out of request URI
         int semicolon = uri.indexOf(MATCH_SESSION_ID);
         if (semicolon >= 0) {
@@ -132,23 +149,21 @@ public class HttpProcessor {
             request.setRequestedSessionId(null);
             request.setRequestedSesssionURL(false);
         }
+        return uri;
+    }
 
-        // Normalize URI (using String operation at the moment)
-        String normalizedUri = normalize(uri);
-        // Set the corresponding request properties.
-        // Transition protect set HttpRequest's properties,not subclass's.
-        // If it have.
-        ((HttpRequest) request).setMethod(method);
-        request.setProtocol(protocol); // I also can't understand this ...
-        if (normalizedUri != null) {
-            ((HttpRequest) request).setRequestURI(normalizedUri);
+    private String parseParameters() {
+        String uri;// Parse any query parameters out of the request URI
+        int question = requestLine.indexOf("?");
+        if (question >= 0) {
+            request.setQueryString(new String(requestLine.uri, question + 1,
+                    requestLine.uriEnd));
+            uri = new String(requestLine.uri, 0, question);
         } else {
-            ((HttpRequest) request).setRequestURI(uri);
+            request.setQueryString(null);
+            uri = new String(requestLine.uri, 0, requestLine.uriEnd);
         }
-
-        if (normalizedUri == null) {
-            throw new ServletException("Invalid URI:" + uri + "'");
-        }
+        return uri;
     }
 
     private String normalize(String uri) {

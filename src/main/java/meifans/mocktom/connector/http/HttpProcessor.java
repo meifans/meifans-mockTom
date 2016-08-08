eabf8cd0a20cd12d4e18f0933a3f73586b651967
赵鹏飞 <pengfeizhao9@creditease.cn>
2016-08-08 17:19:06 +0800

    complete HttpRequestLine and add StrUtil

--- src/main/java/meifans/mocktom/connector/http/HttpProcessor.java
+++ src/main/java/meifans/mocktom/connector/http/HttpProcessor.java
@@ -1,277 +1,259 @@
 package meifans.mocktom.connector.http;
 
+import meifans.mocktom.container.ServletProcessor;
+import meifans.mocktom.container.StaticResourceProcessor;
+import meifans.mocktom.util.RequestUtil;
+import org.apache.tomcat.util.res.StringManager;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-
-import org.apache.tomcat.util.res.StringManager;
-
-import meifans.mocktom.container.ServletProcessor;
-import meifans.mocktom.container.StaticResourceProcessor;
-import meifans.mocktom.util.RequestUtil;
-
 public class HttpProcessor {
 
-    private HttpConnector connector;
-    private HttpRequest request;
-    private HttpResponse response;
-    // 没有完成
-    private HttpRequestLine requestLine = new HttpRequestLine();
+	private static final String MATCH_SESSION_ID = ";jsessionid=";
+	/**
+	 * This string manager for this package
+	 */
+	protected StringManager sm = StringManager.getManager("meifans.mocktom.connector.http");
+	private HttpConnector connector;
+	private HttpRequest request;
+	private HttpResponse response;
+	// 没有完成
+	private HttpRequestLine requestLine = new HttpRequestLine();
 
-    private static final String MATCH_SESSION_ID = ";jsessionid=";
+	public HttpProcessor(HttpConnector connector) {
+		this.connector = connector;
+	}
 
-    /**
-     * This string manager for this package
-     */
-    protected StringManager sm = StringManager
-            .getManager("meifans.mocktom.connector.http");
+	public void process(Socket socket) {
+		SocketInputStream input = null;
+		OutputStream output = null;
+		try {
+			// read 方法重载没有完成
+			// 取得输入输出流
+			input = new SocketInputStream(socket.getInputStream(), 2048);
+			output = socket.getOutputStream();
 
-    public HttpProcessor(HttpConnector connector) {
-        this.connector = connector;
-    }
+			// httpRequest和httpResponse没有完成
+			// 创建，初始化request与response对象
+			request = new HttpRequest(input);
+			response = new HttpResponse(output);
+			response.setRequest(request);
+			response.setHeader("Server", "mockTom Servlet container");
 
-    public void process(Socket socket) {
-        SocketInputStream input = null;
-        OutputStream output = null;
-        try {
-            // read 方法重载没有完成
-            // 取得输入输出流
-            input = new SocketInputStream(socket.getInputStream(), 2048);
-            output = socket.getOutputStream();
+			parseRequest(input, output);
+			pareHeaders(input);// 没有完成
 
-            // httpRequest和httpResponse没有完成
-            // 创建，初始化request与response对象
-            request = new HttpRequest(input);
-            response = new HttpResponse(output);
-            response.setRequest(request);
-            response.setHeader("Server", "mockTom Servlet container");
+			// check if this is a request for a servlet or a static resource
+			// a request for a servlet begin with "/servlet/"
+			if (request.getRequestURI().startsWith("/servlet/")) {
+				ServletProcessor processor = new ServletProcessor();
+				processor.process(request, response);// 没有完成
+			} else {
+				StaticResourceProcessor processor = new StaticResourceProcessor();
+				processor.process(request, response);// 没有完成
+			}
 
-            parseRequest(input, output);
-            pareHeaders(input);// 没有完成
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ServletException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-            // check if this is a request for a servlet or a static resource
-            // a request for a servlet begin with "/servlet/"
-            if (request.getRequestURI().startsWith("/servlet/")) {
-                ServletProcessor processor = new ServletProcessor();
-                processor.process(request, response);// 没有完成
-            } else {
-                StaticResourceProcessor processor = new StaticResourceProcessor();
-                processor.process(request, response);// 没有完成
-            }
+	}
 
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ServletException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+	/**
+	 * @param input
+	 * @param output
+	 * @throws Exception
+	 * @throws ServletException
+	 */
+	private void parseRequest(SocketInputStream input, OutputStream output) throws Exception, ServletException {
 
-    }
+		// Parse the incoming request line
+		input.readRequestLine(requestLine);
+		String method = new String(requestLine.method, 0, requestLine.methodEnd);
+		String uri = null;
+		String protocol = new String(requestLine.protocol, 0, requestLine.protocolEnd);
 
-    /**
-     * @param input
-     * @param output
-     * @throws Exception
-     * @throws ServletException
-     */
-    private void parseRequest(SocketInputStream input, OutputStream output)
-            throws Exception, ServletException {
+		// Validate the incoming request line
+		if (method.length() < 1) {
+			throw new ServletException("Missing HTTP request method ");
+		} else if (requestLine.uriEnd < 1) {
+			throw new ServletException("Missing HTTP request URI");
+		}
+		uri = parseParameters();
+		// Checking for an absolute URI (with the HTTP protocol)
+		if (!uri.startsWith("/")) {
+			int pos = uri.indexOf("://");
+			// Prase out protocol and host name
+			if (pos != -1) {
+				pos = uri.indexOf('/', pos + 3);
+				if (pos == -1) {
+					uri = "";
+				} else {
+					uri = uri.substring(pos);
+				}
+			}
+		}
+		uri = sessionID(uri);
 
-        // Parse the incoming request line
-        input.readRequestLine(requestLine);
-        String method = new String(requestLine.method, 0, requestLine.methodEnd);
-        String uri = null;
-        String protocol = new String(requestLine.protocol, 0, requestLine.protocolEnd);
+		// Normalize URI (using String operation at the moment)
+		String normalizedUri = normalize(uri);
+		// Set the corresponding request properties.
+		// Transition protect set HttpRequest's properties,not subclass's.
+		// If it have.
+		((HttpRequest) request).setMethod(method);
+		request.setProtocol(protocol); // I also can't understand this ...
+		if (normalizedUri != null) {
+			((HttpRequest) request).setRequestURI(normalizedUri);
+		} else {
+			((HttpRequest) request).setRequestURI(uri);
+		}
 
-        // Validate the incoming request line
-        if (method.length() < 1) {
-            throw new ServletException("Missing HTTP request method ");
-        } else if (requestLine.uriEnd < 1) {
-            throw new ServletException("Missing HTTP request URI");
-        }
-        uri = parseParameters();
-        // Checking for an absolute URI (with the HTTP protocol)
-        if (!uri.startsWith("/")) {
-            int pos = uri.indexOf("://");
-            // Prase out protocol and host name
-            if (pos != -1) {
-                pos = uri.indexOf('/', pos + 3);
-                if (pos == -1) {
-                    uri = "";
-                } else {
-                    uri = uri.substring(pos);
-                }
-            }
-        }
-        uri = sessionID(uri);
+		if (normalizedUri == null) {
+			throw new ServletException("Invalid URI:" + uri + "'");
+		}
+	}
 
-        // Normalize URI (using String operation at the moment)
-        String normalizedUri = normalize(uri);
-        // Set the corresponding request properties.
-        // Transition protect set HttpRequest's properties,not subclass's.
-        // If it have.
-        ((HttpRequest) request).setMethod(method);
-        request.setProtocol(protocol); // I also can't understand this ...
-        if (normalizedUri != null) {
-            ((HttpRequest) request).setRequestURI(normalizedUri);
-        } else {
-            ((HttpRequest) request).setRequestURI(uri);
-        }
+	private String sessionID(String uri) {
+		// Parse any requested session Id out of request URI
+		int semicolon = uri.indexOf(MATCH_SESSION_ID);
+		if (semicolon >= 0) {
+			String rest = uri.substring(semicolon + MATCH_SESSION_ID.length());
+			int semicolon2 = rest.indexOf(';');
+			if (semicolon2 > 0) {
+				request.setRequestedSessionId(rest.substring(0, semicolon2));
+				rest = rest.substring(semicolon2);
+			} else {
+				request.setRequestedSessionId(rest);
+				rest = "";
+			}
+			request.setRequestedSessionURL(true);
+			uri = uri.substring(0, semicolon) + rest;
+		} else {
+			request.setRequestedSessionId(null);
+			request.setRequestedSesssionURL(false);
+		}
+		return uri;
+	}
 
-        if (normalizedUri == null) {
-            throw new ServletException("Invalid URI:" + uri + "'");
-        }
-    }
+	private String parseParameters() {
+		String uri;// Parse any query parameters out of the request URI
+		int question = requestLine.indexOf("?");
+		if (question >= 0) {
+			request.setQueryString(new String(requestLine.uri, question + 1, requestLine.uriEnd));
+			uri = new String(requestLine.uri, 0, question);
+		} else {
+			request.setQueryString(null);
+			uri = new String(requestLine.uri, 0, requestLine.uriEnd);
+		}
+		return uri;
+	}
 
-    private String sessionID(String uri) {
-        // Parse any requested session Id out of request URI
-        int semicolon = uri.indexOf(MATCH_SESSION_ID);
-        if (semicolon >= 0) {
-            String rest = uri.substring(semicolon + MATCH_SESSION_ID.length());
-            int semicolon2 = rest.indexOf(';');
-            if (semicolon2 > 0) {
-                request.setRequestedSessionId(rest.substring(0, semicolon2));
-                rest = rest.substring(semicolon2);
-            } else {
-                request.setRequestedSessionId(rest);
-                rest = "";
-            }
-            request.setRequestedSessionURL(true);
-            uri = uri.substring(0, semicolon) + rest;
-        } else {
-            request.setRequestedSessionId(null);
-            request.setRequestedSesssionURL(false);
-        }
-        return uri;
-    }
+	private String normalize(String path) {
+		if (path == null)
+			return null;
+		String normalized = path;
+		// Prevent encoding '%', '/', '.' and '\', which are special reserved
+		// characters
+		if ((normalized.indexOf("%25") >= 0) || (normalized.indexOf("%2F") >= 0) || (normalized.indexOf("%2E") >= 0)
+				|| (normalized.indexOf("%5C") >= 0) || (normalized.indexOf("%2f") >= 0) || (normalized.indexOf("%2e")
+				>= 0) || (normalized.indexOf("%5c") >= 0)) {
+			return null;
+		}
+		if (normalized.startsWith("/%7E") || normalized.startsWith("/%7e"))
+			normalized = "/~" + normalized.substring(4);
+		if (normalized.equals("/."))
+			return "/";
 
-    private String parseParameters() {
-        String uri;// Parse any query parameters out of the request URI
-        int question = requestLine.indexOf("?");
-        if (question >= 0) {
-            request.setQueryString(new String(requestLine.uri, question + 1,
-                    requestLine.uriEnd));
-            uri = new String(requestLine.uri, 0, question);
-        } else {
-            request.setQueryString(null);
-            uri = new String(requestLine.uri, 0, requestLine.uriEnd);
-        }
-        return uri;
-    }
+		normalized = normalized.replace('\\', '/');
+		if (!normalized.startsWith("/"))
+			normalized = "/" + normalized;
+		// Resolve occurrences of "//" in the normalized path
+		while (true) {
+			int index = normalized.indexOf("//");
+			if (index < 0)
+				break;
+			normalized = normalized.substring(0, index) + normalized.substring(index + 1);
+		}
 
-    private String normalize(String path) {
-        if (path == null)
-            return null;
-        String normalized = path;
-        // Prevent encoding '%', '/', '.' and '\', which are special reserved
-        // characters
-        if ((normalized.indexOf("%25") >= 0)
-                || (normalized.indexOf("%2F") >= 0)
-                || (normalized.indexOf("%2E") >= 0)
-                || (normalized.indexOf("%5C") >= 0)
-                || (normalized.indexOf("%2f") >= 0)
-                || (normalized.indexOf("%2e") >= 0)
-                || (normalized.indexOf("%5c") >= 0)) {
-            return null;
-        }
-        if (normalized.startsWith("/%7E") || normalized.startsWith("/%7e"))
-            normalized = "/~" + normalized.substring(4);
-        if (normalized.equals("/."))
-            return "/";
+		// Resolve occurrences of "/./" in the normalized path
+		while (true) {
+			int index = normalized.indexOf("/./");
+			if (index < 0)
+				break;
+			normalized = normalized.substring(0, index) + normalized.substring(index + 2);
+		}
 
-        normalized = normalized.replace('\\', '/');
-        if (!normalized.startsWith("/"))
-            normalized = "/" + normalized;
-        // Resolve occurrences of "//" in the normalized path
-        while (true) {
-            int index = normalized.indexOf("//");
-            if (index < 0)
-                break;
-            normalized = normalized.substring(0, index) +
-                    normalized.substring(index + 1);
-        }
+		// Resolve occurrences of "/../" in the normalized path
+		while (true) {
+			int index = normalized.indexOf("/../");
+			if (index < 0)
+				break;
+			if (index == 0)
+				return (null); // Trying to go outside our context
+			int index2 = normalized.lastIndexOf('/', index - 1);
+			normalized = normalized.substring(0, index2) + normalized.substring(index + 3);
+		}
 
-        // Resolve occurrences of "/./" in the normalized path
-        while (true) {
-            int index = normalized.indexOf("/./");
-            if (index < 0)
-                break;
-            normalized = normalized.substring(0, index) +
-                    normalized.substring(index + 2);
-        }
+		// Declare occurrences of "/..." (three or more dots) to be invalid
+		// (on some Windows platforms this walks the directory tree!!!)
+		if (normalized.indexOf("/...") >= 0)
+			return (null);
 
-        // Resolve occurrences of "/../" in the normalized path
-        while (true) {
-            int index = normalized.indexOf("/../");
-            if (index < 0)
-                break;
-            if (index == 0)
-                return (null); // Trying to go outside our context
-            int index2 = normalized.lastIndexOf('/', index - 1);
-            normalized = normalized.substring(0, index2) +
-                    normalized.substring(index + 3);
-        }
+		// Return the normalized path that we have completed
+		return (normalized);
 
-        // Declare occurrences of "/..." (three or more dots) to be invalid
-        // (on some Windows platforms this walks the directory tree!!!)
-        if (normalized.indexOf("/...") >= 0)
-            return (null);
+	}
 
-        // Return the normalized path that we have completed
-        return (normalized);
-
-    }
-
-    private void pareHeaders(SocketInputStream input)
-            throws IOException, ServletException {
-        while (true) {
-            HttpHeader header = new HttpHeader();
-            // Read the next header
-            input.readHeader(header);
-            if (header.nameEnd == 0) {
-                if (header.valueEnd == 0) {
-                    return;
-                } else {
-                    throw new ServletException(
-                            sm.getString("httpProcessor.parseHeader.colon"));
-                }
-            }
-            String name = new String(header.name, 0, header.nameEnd);
-            String value = new String(header.value, 0, header.valueEnd);
-            request.addHeader(name, value);
-            // do something for some headers,ignore others.
-            if (name.equals("cookie")) {
-                Cookie cookies[] = RequestUtil.parseCookieHeader(value);
-                for (int i = 0; i < cookies.length; i++) {
-                    if (cookies[i].getName().equals("jsession")) {
-                        // Override anything requested in the URL
-                        if (!request.isRequestedSessionIdFromCookie()) {
-                            // Accept only the first session id cookie
-                            request.setRequestedSessionId(cookies[i].getValue());
-                            request.setRequestedSessionCookie(true);
-                            request.setRequestedSessionURL(false);
-                        }
-                    }
-                    request.addCookie(cookies[i]);
-                }
-            } else if (name.equals("content-length")) {
-                int n = -1;
-                try {
-                    n = Integer.parseInt(value);
-                } catch (Exception e) {
-                    throw new ServletException(
-                            sm.getString("httpProcessor.parseHeaders.contentLength"));
-                }
-                request.setContentLength(n);
-            } else if (name.equals("content-type")) {
-                request.setContentType(value);
-            }
-        } // end while
-    }
+	private void pareHeaders(SocketInputStream input) throws IOException, ServletException {
+		while (true) {
+			HttpHeader header = new HttpHeader();
+			// Read the next header
+			input.readHeader(header);
+			if (header.nameEnd == 0) {
+				if (header.valueEnd == 0) {
+					return;
+				} else {
+					throw new ServletException(sm.getString("httpProcessor.parseHeader.colon"));
+				}
+			}
+			String name = new String(header.name, 0, header.nameEnd);
+			String value = new String(header.value, 0, header.valueEnd);
+			request.addHeader(name, value);
+			// do something for some headers,ignore others.
+			if (name.equals("cookie")) {
+				Cookie cookies[] = RequestUtil.parseCookieHeader(value);
+				for (int i = 0; i < cookies.length; i++) {
+					if (cookies[i].getName().equals("jsession")) {
+						// Override anything requested in the URL
+						if (!request.isRequestedSessionIdFromCookie()) {
+							// Accept only the first session id cookie
+							request.setRequestedSessionId(cookies[i].getValue());
+							request.setRequestedSessionCookie(true);
+							request.setRequestedSessionURL(false);
+						}
+					}
+					request.addCookie(cookies[i]);
+				}
+			} else if (name.equals("content-length")) {
+				int n = -1;
+				try {
+					n = Integer.parseInt(value);
+				} catch (Exception e) {
+					throw new ServletException(sm.getString("httpProcessor.parseHeaders.contentLength"));
+				}
+				request.setContentLength(n);
+			} else if (name.equals("content-type")) {
+				request.setContentType(value);
+			}
+		} // end while
+	}
 
 }
--- src/main/java/meifans/mocktom/connector/http/HttpRequestLine.java
+++ src/main/java/meifans/mocktom/connector/http/HttpRequestLine.java
@@ -1,78 +1,67 @@
 package meifans.mocktom.connector.http;
 
-import java.util.Objects;
+import meifans.mocktom.util.StrUtil;
 
 /**
  * Http request line enum type
- * 
- * @author meifans
  *
+ * @author meifans
  */
 final class HttpRequestLine {
 
-    // ----------------------------------------------------------- Contants
-    public static final int INITIAL_METHOD_SIZE = 8;
-    public static final int INITIAL_URI_SIZE = 64;
-    public static final int INITIAL_PROTOCOL_SIZE = 8;
-    public static final int MAX_METHOD_SIZE = 1024;
-    public static final int MAX_URI_SIZE = 32768;
-    public static final int MAX_PROTOCOL_SIZE = 1024;
-    // --------------------------------------------------------- Constructors
-    public HttpRequestLine(){
-        this(new char[INITIAL_METHOD_SIZE],0,
-                new char[INITIAL_URI_SIZE],0,
-                    new char[INITIAL_PROTOCOL_SIZE],0);
-    }
+	public static final int INITIAL_METHOD_SIZE = 8;
+	public static final int INITIAL_URI_SIZE = 64;
+	public static final int INITIAL_PROTOCOL_SIZE = 8;
+	public static final int MAX_METHOD_SIZE = 1024;
+	public static final int MAX_URI_SIZE = 32768;
+	public static final int MAX_PROTOCOL_SIZE = 1024;
 
-    public HttpRequestLine(char[] method,int methodEnd,
-                           char[] uri,int uriEnd,
-                           char[] protocol,int protocolEnd){
-        this.method = method;
-        this.methodEnd = methodEnd;
-        this.uri = uri;
-        this.uriEnd = uriEnd;
-        this.protocol = protocol;
-        this.protocolEnd = protocolEnd;
-    }
+	public char[] uri;
+	public int uriEnd;
+	public char[] protocol;
+	public int protocolEnd;
+	public char[] method;
+	public int methodEnd;
 
-    // ----------------------------------------------------- Instance Variables
+	public HttpRequestLine() {
+		this(new char[INITIAL_METHOD_SIZE], 0, new char[INITIAL_URI_SIZE], 0, new char[INITIAL_PROTOCOL_SIZE], 0);
+	}
 
-    public char[] uri = null;
-    public int uriEnd = 0;
-    public char[] protocol = null;
-    public int protocolEnd = 0;
-    public char[] method = null;
-    public int methodEnd = 0;
+	public HttpRequestLine(char[] method, int methodEnd, char[] uri, int uriEnd, char[] protocol, int protocolEnd) {
+		this.method = method;
+		this.methodEnd = methodEnd;
+		this.uri = uri;
+		this.uriEnd = uriEnd;
+		this.protocol = protocol;
+		this.protocolEnd = protocolEnd;
+	}
 
-    /**
-     * Release all object references,and initialize instance variables,in
-     * preparation for reuse of  this object.
-     */
-    public void recycle(){
-        methodEnd=0;
-        uriEnd=0;
-        protocolEnd=0;
-    }
+	/**
+	 * Release all object references,and initialize instance variables,in
+	 * preparation for reuse of  this object.
+	 */
+	public void recycle() {
+		methodEnd = 0;
+		uriEnd = 0;
+		protocolEnd = 0;
+	}
 
-    /**
-     * Test if the value of the header includes the given char array.
-     * @param buf
-     * @return
-     */
-    public int indexOf(char[] buf) {
-        return indexof(buf,buf.length);
-    }
+	/**
+	 * Test if the value of the header includes the given char array.
+	 *
+	 * @param buf
+	 * @return
+	 */
+	public int indexOf(char[] buf) {
+		return indexOf(buf, buf.length);
+	}
 
-    private int indexof(char[] buf, int end) {
-        return 0;
-    }
+	public int indexOf(String s) {
+		return indexOf(s.toCharArray());
+	}
 
-    public int hashCode(){
-        //FixMe
-        return 0;
-    }
+	private int indexOf(char[] buf, int end) {
+		return StrUtil.indexOf(uri, buf, 0, end);
+	}
 
-    public boolean equals(Object obj){
-        return false;
-    }
 }
